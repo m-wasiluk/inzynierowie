8e2c892deca4cae7722e58d8bd846cbe640f355b
Utsitave <utsitavets@gmail.com>
2024-04-21 04:23:32 +0200

    buzzer+ zip biblioteka + kod do r2 poczatek

--- arduino_reciver/arduino_reciver.ino
+++ arduino_reciver/arduino_reciver.ino
@@ -9,7 +9,7 @@ int keyIndex = 0;
 
 int status = WL_IDLE_STATUS;
 
-char server[] = "http://192.168.4.1/ZAngle";
+char server[] = "http://192.168.4.1/Data";
 WiFiSSLClient client;
 
 void setup() {
@@ -22,29 +22,17 @@ void setup() {
  
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
- 
     while (true);
   }
-
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-
   // attempt to connect to WiFi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-  
     status = WiFi.begin(ssid, pass);
-
-  
-
     delay(10000);
   }
   Serial.println("Connected to WiFi");
   printWiFiStatus();
-
   Serial.println("\nStarting connection to server...");
   if (client.connect(server, 443)) {
     Serial.println("connected to server");
@@ -53,24 +41,54 @@ void setup() {
   }
 }
 
+
+void Show_meme() {
+}
+
+void Drive (int x, int y) {
+  
+
+}
+
 void loop() {
+  String buffer = "";
+  int x;
+  int y;
+  int button;
+  int i=0;
   while (client.available()) {
     char c = client.read();
-    Serial.write(c);
+    buffer += c;
   }
-
-  if (!client.connected()) {
-    Serial.println();
-
-
-    ,
-    AQSW2
-    Serial.println("disconnecting from server.");
-    client.stop();
-
-    while (true);
+  string k="";
+  char* pom=buffer;
+  while(*pom != '$'){
+    k += *pom;
+    pom++;
   }
-}
+  int x =int(k);
+
+  k="";
+  pom++;
+  while(*pom != '$'){
+    k += *pom;
+    pom++;
+  }
+  int y =int(k);
+
+  k="";
+  pom++;
+  while(*pom != '$'){
+    k += *pom;
+    pom++;
+  }
+  int button =int(k);
+
+  if(button == 1){
+    Show_meme();
+  }
+  Drive(x, y);
+  
 
 
 void printWiFiStatus() {
